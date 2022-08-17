2d99481b66b0e207a4ec1a6d279621f3f8268c2b
Damian Jamroży <damianjamrozy99@gmail.com>
2022-08-17 13:55:39 +0200

    Change style

--- Index.php
+++ Index.php
@@ -10,10 +10,67 @@
 <?php include("./generator/header.php");?>
 
 <div class="container" id="main-content">
-	<center><img src="files\img\primary\logov2.png" width="25%">
-	<p class="blue">All Likes Impressive Personal Site</p>
+	<div class="left-side"></div>
 
-	<p style="margin-top:4em;">Poznaj świat naszymi oczami!</p>
+	<div class="right-side">
+		
+		<h4> Poznaj świat naszymi oczami! </h4><br>
+		<p>Główne założenia aplikacji to:</p><br>
+
+		<div class="circle_box">
+			<div class="circle_container">
+				<div class="circle_main">
+					<div class="circle_text_container">
+						<div class = "circle_text">
+							1
+						</div>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<div class="circle_text_box">
+			Logic games
+		</div>
+
+		<div class="circle_box">
+			<div class="circle_container">
+				<div class="circle_main">
+					<div class="circle_text_container">
+						<div class = "circle_text">
+							2
+						</div>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<div class="circle_text_box">
+			Eye tracking
+		</div>
+
+		<div class="circle_box">
+			<div class="circle_container">
+				<div class="circle_main">
+					<div class="circle_text_container">
+						<div class = "circle_text">
+							3
+						</div>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<div class="circle_text_box">
+			Speach recognition
+		</div>
+
+		<p>
+			<button class="glow-on-hover btn-down" type="button">Zaczynajmy</button>
+		</p>
+	</div>
+
+	<!-- <center><img src="files\img\primary\logov2.png" width="25%">
+	<p class="blue">All Likes Impressive Personal Site</p> -->
+
+	<!-- <p style="margin-top:4em;">Poznaj świat naszymi oczami!</p>
 	
 	<p style="margin-top:4em;">
 		<button class="glow-on-hover" type="button">Zaczynajmy</button>
@@ -23,7 +80,7 @@
 		<button class="glow-on-hover" type="button">Pomiń</button>
 	</p>
 
-	</center>
+	</center> -->
 </div>
 <script src="./js/index.js"></script>
 <?php include("./generator/footer.php");?>

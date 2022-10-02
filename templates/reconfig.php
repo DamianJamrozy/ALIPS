de78f7b864b611ad352ba463ab40e8b8516ef994
Damian Jamroży <damianjamrozy99@gmail.com>
2022-10-02 19:44:05 +0200

    0.12a - Eye config (hide text)

--- templates/reconfig.php
+++ templates/reconfig.php
@@ -50,27 +50,27 @@
 		let c = 'con'; // ID START NAME
 
 		document.getElementById('parallax').innerHTML = `<div class="central-container-side">
-			<div class="grid conactive" id='con1'>1a</div>	<div class="grid">2a</div>	<div class="grid">3a</div>	<div class="grid">4a</div>	<div class="grid" id='con2'>5a</div>
-			<div class="grid" id='con6'>1b</div>	<div class="grid">2b</div>	<div class="grid">3b</div>	<div class="grid">4b</div>	<div class="grid" id='con7'>5b</div>
-			<div class="grid">1c</div>	<div class="grid">2c</div>	<div class="grid">3c</div>	<div class="grid">4c</div>	<div class="grid">5c</div>
-			<div class="grid">1d</div>	<div class="grid">2d</div>	<div class="grid">3d</div>	<div class="grid">4d</div>	<div class="grid">5d</div>
-			<div class="grid">1e</div>	<div class="grid">2e</div>	<div class="grid" id='con5'>3e</div>	<div class="grid">4e</div>	<div class="grid">5e</div>
-			<div class="grid">1f</div>	<div class="grid">2f</div>	<div class="grid" id='con10'>3f</div>	<div class="grid">4f</div>	<div class="grid">5f</div>
-			<div class="grid">1g</div>	<div class="grid">2g</div>	<div class="grid">3g</div>	<div class="grid">4g</div>	<div class="grid">5g</div>
-			<div class="grid">1h</div>	<div class="grid">2h</div>	<div class="grid">3h</div>	<div class="grid">4h</div>	<div class="grid">5h</div>
-			<div class="grid" id='con3'>1i</div>	<div class="grid">2i</div>	<div class="grid">3i</div>	<div class="grid">4i</div>	<div class="grid" id='con4'>5i</div>
-			<div class="grid" id='con8'>1j</div>	<div class="grid">2j</div>	<div class="grid">3j</div>	<div class="grid">4j</div>	<div class="grid" id='con9'>5j</div>
+			<div class="grid conactive" id='con1'></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con2'></div>
+			<div class="grid" id='con6'></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con7'></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con5'></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con10'></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid" id='con3'></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con4'></div>
+			<div class="grid" id='con8'></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con9'></div>
 
-			<div class="grid" id='con11'>1a</div>	<div class="grid">2a</div>	<div class="grid">3a</div>	<div class="grid">4a</div>	<div class="grid" id='con12'>5a</div>
-			<div class="grid" id='con16'>1b</div>	<div class="grid">2b</div>	<div class="grid">3b</div>	<div class="grid">4b</div>	<div class="grid" id='con17'>5b</div>
-			<div class="grid">1c</div>	<div class="grid">2c</div>	<div class="grid">3c</div>	<div class="grid">4c</div>	<div class="grid">5c</div>
-			<div class="grid">1d</div>	<div class="grid">2d</div>	<div class="grid">3d</div>	<div class="grid">4d</div>	<div class="grid">5d</div>
-			<div class="grid">1e</div>	<div class="grid">2e</div>	<div class="grid" id='con15'>3e</div>	<div class="grid">4e</div>	<div class="grid">5e</div>
-			<div class="grid">1f</div>	<div class="grid">2f</div>	<div class="grid" id='con20'>3f</div>	<div class="grid">4f</div>	<div class="grid">5f</div>
-			<div class="grid">1g</div>	<div class="grid">2g</div>	<div class="grid">3g</div>	<div class="grid">4g</div>	<div class="grid">5g</div>
-			<div class="grid">1h</div>	<div class="grid">2h</div>	<div class="grid">3h</div>	<div class="grid">4h</div>	<div class="grid">5h</div>
-			<div class="grid" id='con13'>1i</div>	<div class="grid">2i</div>	<div class="grid">3i</div>	<div class="grid">4i</div>	<div class="grid" id='con14'>5i</div>
-			<div class="grid" id='con18'>1j</div>	<div class="grid">2j</div>	<div class="grid">3j</div>	<div class="grid">4j</div>	<div class="grid" id='con19'>5j</div>
+			<div class="grid" id='con11'></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con12'></div>
+			<div class="grid" id='con16'></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con17'></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con15'></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con20'></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>
+			<div class="grid" id='con13'></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con14'></div>
+			<div class="grid" id='con18'></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid"></div>	<div class="grid" id='con19'></div>
 		</div>`;
 
 		// Create new script element
@@ -128,14 +128,25 @@
 
 
 <!-- <div class="central-container-side">
-		<div class="grid">1a</div>	<div class="grid">2a</div>	<div class="grid">3a</div>	<div class="grid">4a</div>	<div class="grid">5a</div>
-		<div class="grid">1b</div>	<div class="grid">2b</div>	<div class="grid">3b</div>	<div class="grid">4b</div>	<div class="grid">5b</div>
-		<div class="grid">1c</div>	<div class="grid">2c</div>	<div class="grid">3c</div>	<div class="grid">4c</div>	<div class="grid">5c</div>
-		<div class="grid">1d</div>	<div class="grid">2d</div>	<div class="grid">3d</div>	<div class="grid">4d</div>	<div class="grid">5d</div>
-		<div class="grid">1e</div>	<div class="grid">2e</div>	<div class="grid">3e</div>	<div class="grid">4e</div>	<div class="grid">5e</div>
-		<div class="grid">1f</div>	<div class="grid">2f</div>	<div class="grid">3f</div>	<div class="grid">4f</div>	<div class="grid">5f</div>
-		<div class="grid">1g</div>	<div class="grid">2g</div>	<div class="grid">3g</div>	<div class="grid">4g</div>	<div class="grid">5g</div>
-		<div class="grid">1h</div>	<div class="grid">2h</div>	<div class="grid">3h</div>	<div class="grid">4h</div>	<div class="grid">5h</div>
-		<div class="grid">1i</div>	<div class="grid">2i</div>	<div class="grid">3i</div>	<div class="grid">4i</div>	<div class="grid">5i</div>
-		<div class="grid">1j</div>	<div class="grid">2j</div>	<div class="grid">3j</div>	<div class="grid">4j</div>	<div class="grid">5j</div>
+		<div class="grid conactive" id='con1'>1a</div>	<div class="grid">2a</div>	<div class="grid">3a</div>	<div class="grid">4a</div>	<div class="grid" id='con2'>5a</div>
+			<div class="grid" id='con6'>1b</div>	<div class="grid">2b</div>	<div class="grid">3b</div>	<div class="grid">4b</div>	<div class="grid" id='con7'>5b</div>
+			<div class="grid">1c</div>	<div class="grid">2c</div>	<div class="grid">3c</div>	<div class="grid">4c</div>	<div class="grid">5c</div>
+			<div class="grid">1d</div>	<div class="grid">2d</div>	<div class="grid">3d</div>	<div class="grid">4d</div>	<div class="grid">5d</div>
+			<div class="grid">1e</div>	<div class="grid">2e</div>	<div class="grid" id='con5'>3e</div>	<div class="grid">4e</div>	<div class="grid">5e</div>
+			<div class="grid">1f</div>	<div class="grid">2f</div>	<div class="grid" id='con10'>3f</div>	<div class="grid">4f</div>	<div class="grid">5f</div>
+			<div class="grid">1g</div>	<div class="grid">2g</div>	<div class="grid">3g</div>	<div class="grid">4g</div>	<div class="grid">5g</div>
+			<div class="grid">1h</div>	<div class="grid">2h</div>	<div class="grid">3h</div>	<div class="grid">4h</div>	<div class="grid">5h</div>
+			<div class="grid" id='con3'>1i</div>	<div class="grid">2i</div>	<div class="grid">3i</div>	<div class="grid">4i</div>	<div class="grid" id='con4'>5i</div>
+			<div class="grid" id='con8'>1j</div>	<div class="grid">2j</div>	<div class="grid">3j</div>	<div class="grid">4j</div>	<div class="grid" id='con9'>5j</div>
+
+			<div class="grid" id='con11'>1a</div>	<div class="grid">2a</div>	<div class="grid">3a</div>	<div class="grid">4a</div>	<div class="grid" id='con12'>5a</div>
+			<div class="grid" id='con16'>1b</div>	<div class="grid">2b</div>	<div class="grid">3b</div>	<div class="grid">4b</div>	<div class="grid" id='con17'>5b</div>
+			<div class="grid">1c</div>	<div class="grid">2c</div>	<div class="grid">3c</div>	<div class="grid">4c</div>	<div class="grid">5c</div>
+			<div class="grid">1d</div>	<div class="grid">2d</div>	<div class="grid">3d</div>	<div class="grid">4d</div>	<div class="grid">5d</div>
+			<div class="grid">1e</div>	<div class="grid">2e</div>	<div class="grid" id='con15'>3e</div>	<div class="grid">4e</div>	<div class="grid">5e</div>
+			<div class="grid">1f</div>	<div class="grid">2f</div>	<div class="grid" id='con20'>3f</div>	<div class="grid">4f</div>	<div class="grid">5f</div>
+			<div class="grid">1g</div>	<div class="grid">2g</div>	<div class="grid">3g</div>	<div class="grid">4g</div>	<div class="grid">5g</div>
+			<div class="grid">1h</div>	<div class="grid">2h</div>	<div class="grid">3h</div>	<div class="grid">4h</div>	<div class="grid">5h</div>
+			<div class="grid" id='con13'>1i</div>	<div class="grid">2i</div>	<div class="grid">3i</div>	<div class="grid">4i</div>	<div class="grid" id='con14'>5i</div>
+			<div class="grid" id='con18'>1j</div>	<div class="grid">2j</div>	<div class="grid">3j</div>	<div class="grid">4j</div>	<div class="grid" id='con19'>5j</div>
 	</div> -->
